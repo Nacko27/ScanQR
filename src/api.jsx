295964942573083ff8d68760e337e6f8b13e73bc
Nacko27 <nikhiljambhulkar239@gmail.com>
2024-01-07 15:04:32 +0530

    first commit

---
+++ src/api.jsx
@@ -0,0 +1,7 @@
+const qrServerBaseUrl = 'https://api.qrserver.com/v1/create-qr-code/';
+
+export const generateQRCode = (qrData, qrSize) => {
+  const qrImageUrl = `${qrServerBaseUrl}?data=${encodeURIComponent(qrData)}&size=${qrSize}`;
+  return qrImageUrl;
+};
+
